4a99eba1c596a324003d09844670066cb9ae34d4
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2022-12-15 02:13:52 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -52,12 +52,12 @@ function App() {
                 <div className="description">
                   <div>
                     <p>{item.url}</p>
-                    <a target="_blank" href={item.url}>
+                    <a target="_blank" rel="noreferrer" href={item.url}>
                       <h1>{item.title}</h1>
                     </a>
                     <p>{item.description}</p>
                   </div>
-                  <img width={100} height={100} src={item.urlToImage} />
+                  <img width={100} height={100} src={item.urlToImage} alt="news image" />
                 </div>
               </div>
             );
